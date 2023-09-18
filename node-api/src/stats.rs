405798c6e5a225d1ef52a1949192c2f5df46b1c6
Zola Gonano <zolagonano@protonmail.com>
2023-09-18 11:30:12 +0000

    Implement Server stats

---
+++ node-api/src/stats.rs
@@ -0,0 +1,205 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
+use systemstat::{ByteSize, Platform};
+
+#[derive(Deserialize, Serialize)]
+pub struct NetworkUsage {
+    interface: String,
+    tx: String,
+    rx: String,
+    total: String,
+}
+
+impl NetworkUsage {
+    pub fn get_net_if() -> Vec<String> {
+        let paths = fs::read_dir("/sys/class/net").unwrap();
+
+        let mut interfaces: Vec<String> = Vec::new();
+
+        for path in paths {
+            interfaces.push(
+                path.unwrap()
+                    .path()
+                    .display()
+                    .to_string()
+                    .replace("/sys/class/net/", ""),
+            );
+        }
+
+        interfaces
+    }
+
+    pub fn get_usage() -> Vec<NetworkUsage> {
+        let mut network_usage_list: Vec<NetworkUsage> = Vec::new();
+
+        for interface in NetworkUsage::get_net_if() {
+            let interface_rx: u64 =
+                fs::read_to_string(format!("/sys/class/net/{interface}/statistics/rx_bytes"))
+                    .unwrap()
+                    .trim()
+                    .parse()
+                    .unwrap(); /* It can be unwrapped, we won't get errors here */
+
+            let interface_tx: u64 =
+                fs::read_to_string(format!("/sys/class/net/{interface}/statistics/tx_bytes"))
+                    .unwrap()
+                    .trim()
+                    .parse()
+                    .unwrap();
+
+            network_usage_list.push(NetworkUsage::new(interface, interface_tx, interface_rx));
+        }
+
+        network_usage_list
+    }
+    pub fn new(interface: String, tx: u64, rx: u64) -> NetworkUsage {
+        let total: u64 = tx + rx;
+
+        NetworkUsage {
+            interface,
+            tx: ByteSize(tx).to_string(),
+            rx: ByteSize(rx).to_string(),
+            total: ByteSize(total).to_string(),
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct UptimeInfo {
+    seconds: u64,
+    pretty: String,
+}
+
+impl UptimeInfo {
+    pub fn new(seconds: u64) -> UptimeInfo {
+        UptimeInfo {
+            seconds,
+            pretty: UptimeInfo::seconds_to_pretty(seconds),
+        }
+    }
+
+    fn seconds_to_pretty(seconds: u64) -> String {
+        let mut time_string = String::new();
+        let mut _remaining = seconds;
+
+        //up 4 days, 15 hours, 22 minutes
+        let months: u64 = _remaining / 2592000;
+        _remaining -= months * 2592000;
+
+        let weeks: u64 = _remaining / 604800;
+        _remaining -= weeks / 604800;
+
+        let days: u64 = _remaining / 86400;
+        _remaining -= days * 86400;
+
+        let hours: u64 = _remaining / 3600;
+        _remaining -= hours * 3600;
+
+        let minutes: u64 = _remaining / 60;
+        _remaining -= minutes * 60;
+
+        if months > 0 {
+            time_string.push_str(&format!("{months} months "));
+        }
+        if weeks > 0 {
+            time_string.push_str(&format!("{weeks} weeks "));
+        }
+        if days > 0 {
+            time_string.push_str(&format!("{days} days "));
+        }
+        if hours > 0 {
+            time_string.push_str(&format!("{hours} hours "));
+        }
+        if minutes > 0 {
+            time_string.push_str(&format!("{minutes} minutes"));
+        }
+
+        time_string
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct MemInfo {
+    used: u64,
+    free: u64,
+    total: u64,
+    pretty: String,
+}
+
+impl MemInfo {
+    pub fn new(total: u64, free: u64) -> MemInfo {
+        let used = total - free;
+        let pretty = format!(
+            "{}/{} ({})",
+            ByteSize(total - free),
+            ByteSize(total),
+            ByteSize(free)
+        );
+        MemInfo {
+            used,
+            free,
+            total,
+            pretty,
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct DiskInfo {
+    mount_point: String,
+    used: u64,
+    free: u64,
+    total: u64,
+    pretty: String,
+}
+
+impl DiskInfo {
+    pub fn new(mount_point: String, total: u64, free: u64) -> DiskInfo {
+        let used = total - free;
+        let pretty = format!(
+            "{}: {}/{} ({})",
+            mount_point,
+            ByteSize(total - free),
+            ByteSize(total),
+            ByteSize(free)
+        );
+        DiskInfo {
+            mount_point,
+            used,
+            free,
+            total,
+            pretty,
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct HwUsage {
+    cpu_load: (f32, f32, f32),
+    memory_usage: MemInfo,
+    swap_usage: MemInfo,
+    disk_info: DiskInfo,
+    uptime: UptimeInfo,
+}
+
+impl HwUsage {
+    pub fn new() -> HwUsage {
+        let sys = systemstat::platform::linux::PlatformImpl::new();
+
+        // TODO: needs some error handling
+        let cpu_load = sys.load_average().unwrap();
+        let memory_usage = sys.memory().unwrap();
+        let swap_usage = sys.swap().unwrap();
+        let uptime = sys.uptime().unwrap();
+        let disk = sys.mount_at("/").unwrap(); // probably would need root previlage
+
+        HwUsage {
+            cpu_load: (cpu_load.one, cpu_load.five, cpu_load.fifteen),
+            memory_usage: MemInfo::new(memory_usage.total.as_u64(), memory_usage.free.as_u64()),
+            swap_usage: MemInfo::new(swap_usage.total.as_u64(), swap_usage.free.as_u64()),
+            uptime: UptimeInfo::new(uptime.as_secs()),
+            disk_info: DiskInfo::new(disk.fs_mounted_on, disk.total.as_u64(), disk.free.as_u64()),
+        }
+    }
+}
+
